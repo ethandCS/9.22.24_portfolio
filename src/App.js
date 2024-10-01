fe5b04fe75503b4cc994a8fd9b297f18c53db05a
ethandCS <112108721+ethandCS@users.noreply.github.com>
2024-10-01 10:47:03 -0700

    fixing style techniques

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import FireCalculator from './components/FireCalculator';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/9.22.24_portfolio">
       <div className="App">
         <header className="App-header">
           <nav>
-            <Link to="/">Home</Link> | <Link to="/calculator">FIRE Calculator</Link>
+            <Link to="/">Home</Link> | 
+            <Link to="/calculator">FIRE Calculator</Link> | 
+            <a href="https://github.com/ethandCS/FIRE_Calculator" target="_blank" rel="noopener noreferrer">
+              GitHub Repo
+            </a>
           </nav>
         </header>
 
